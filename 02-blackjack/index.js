c45d96f6cc485b16c937058b316345fa882dc3ae
9insane9 <145199591+9insane9@users.noreply.github.com>
2023-11-24 17:31:04 +0200

    fix... stuff

--- 02-blackjack/index.js
+++ 02-blackjack/index.js
@@ -1,26 +1,27 @@
-function randomCard() {
-    let bjArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    let randomIndex = Math.floor(Math.random() * bjArray.length);
-    let result = bjArray[randomIndex];
-    console.log(result);
-    return result;
-};
-
-
-
 let hasBlackJack = false;
 let isAlive = false;
 let sum = 0;
-
 let message = "";
-
 let allCurrentCards = [];
+
+let player = {
+    alias: "Gunner",
+    chips: 145 
+}
+
+let playerEl = document.querySelector("#player-el");
+playerEl.textContent = `${player.alias}: $${player.chips}`;
 
 let startButtonEl = document.querySelector("#start-button");
 let messageEl = document.querySelector("#message-el");
 let cardsEl = document.querySelector("#cards-el");
 let sumEl = document.querySelector("#sum-el");
 let newCardButtonEl = document.querySelector("#newcard-button");
+
+startButtonEl.addEventListener("click", startGame);
+newCardButtonEl.addEventListener("click", newCard);
+
+
 
 function startGame() {
     if (!isAlive) {
@@ -31,9 +32,9 @@ function startGame() {
     let secondCard = randomCard();
     allCurrentCards = [firstCard, secondCard]
     sum = firstCard + secondCard;
-   
     renderGame();
-    } else if (isAlive) {
+
+    } else if (isAlive && !hasBlackJack) {
         messageEl.textContent = "Game already in progress!"
     }
 };
@@ -52,25 +53,34 @@ function renderGame() {
         } else if (sum === 21) {
             message = "Wooooohoo! You've got Blackjack bebe!";
             hasBlackJack = true;
+            isAlive = false;//test
         } else { 
             message = "You're out of the game!";
             isAlive = false;
         };
+        
         messageEl.textContent = message;
 };
 
 function newCard() {
-    if (!isAlive) {
-        messageEl.textContent = "No more cards for you!";
+    if (hasBlackJack && !isAlive) {
+        messageEl.textContent = "You've already won!";
     } else if (isAlive && !hasBlackJack) {
-    let card = randomCard();
-    sum += card;
-    allCurrentCards.push(card);
-    renderGame();
-    } else if (hasBlackJack){
-        messageEl.textContent = "You've already won!"
+        let card = randomCard();
+        sum += card;
+        allCurrentCards.push(card);
+        renderGame();
+    } else if (sum === 0) {
+        messageEl.textContent = "Start the game first!"; 
+    } else if (!isAlive){
+        messageEl.textContent = "No more cards for you!";
     }
 };
 
-startButtonEl.addEventListener("click", startGame);
-newCardButtonEl.addEventListener("click", newCard);
+function randomCard() {
+    let bjArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    let randomIndex = Math.floor(Math.random() * bjArray.length);
+    let result = bjArray[randomIndex];
+    console.log(result);
+    return result;
+};
